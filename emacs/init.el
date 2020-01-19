2ea12e04227ab46ac243b950debd3e8997210c1b
Geoff MacIntosh <geoff@mac.into.sh>
2020-01-19 19:36:58 -0330

    Initial commit

---
+++ emacs/init.el
@@ -0,0 +1,21 @@
+(let ((gc-cons-threshold (* 5000 1024 1024))
+      (debug-on-error t))
+
+  (setq straight-use-package-by-default t)
+  
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+  (org-babel-load-file
+   (expand-file-name "emacs.org" user-emacs-directory))
+  (garbage-collect))
---
+++ emacs/straight/versions/default.el
@@ -0,0 +1,6 @@
+(("emacsmirror-mirror" . "d4c9105e5a7b8c9445b2b0903ac43443341e6e4c")
+ ("gnu-elpa-mirror" . "b58580c57aab8a451d2db4956c5d5c191ddcb2a6")
+ ("melpa" . "1b15a29bbd36919b637e3f2aa56bf3cdbff15ee1")
+ ("straight.el" . "7cd0d88b0a1877536439ddd7c1d1b7f3206bedff")
+ ("use-package" . "4b58ab78177f636f862a66c7a8fdcf9b070e0925"))
+:alpha
