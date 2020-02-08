cb3c788f1e0d46c1ff79327ad83ab1b4c381722d
Geoff MacIntosh <geoff@mac.into.sh>
2020-02-08 16:27:46 -0330

    Collect garbage outside of let

--- emacs/init.el
+++ emacs/init.el
@@ -3,20 +3,21 @@
       (debug-on-error t))
 
   (setq straight-use-package-by-default t)
-  
+  (setq load-prefer-newer t)
+
   (defvar bootstrap-version)
   (let ((bootstrap-file
 	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
 	(bootstrap-version 5))
     (unless (file-exists-p bootstrap-file)
       (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
 	(goto-char (point-max))
 	(eval-print-last-sexp)))
     (load bootstrap-file nil 'nomessage))
 
   (org-babel-load-file
-   (expand-file-name "emacs.org" user-emacs-directory))
-  (garbage-collect))
+   (expand-file-name "emacs.org" user-emacs-directory)))
+(garbage-collect)
