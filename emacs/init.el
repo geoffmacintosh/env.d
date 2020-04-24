2264e064085f93d84bdc875e9d2b4b6b01e48f71
Geoff MacIntosh <geoff@mac.into.sh>
2020-04-24 19:19:02 -0230

    Add straight.el feature to install org only once
    https://github.com/raxod502/straight.el/issues/352

--- emacs/init.el
+++ emacs/init.el
@@ -7,26 +7,26 @@
 Should be a drop-in replacement in absolutely all cases. Use
 identically to setq, setting VARIABLE to VALUE."
     `(funcall (or (get ',variable 'custom-set)
-		  'set-default)
-	      ',variable ,value))
+                  'set-default)
+              ',variable ,value))
 
   (defvar bootstrap-version)
   (let ((bootstrap-file
-	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	(bootstrap-version 5))
+         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 5))
     (unless (file-exists-p bootstrap-file)
       (with-current-buffer
-	  (url-retrieve-synchronously
-	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	   'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
     (load bootstrap-file nil 'nomessage))
 
   (straight-use-package 'use-package)
   (use-package no-littering
     :straight t)
-  (straight-use-package 'org-plus-contrib)
+  (straight-use-package '(org-plus-contrib :includes (org)))
   (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
   ;;(load (expand-file-name "actuator.el" user-emacs-directory))
   (garbage-collect))
