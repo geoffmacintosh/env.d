4266f2bf73a3e6c2f6373dc7626308de08e502a8
Geoff MacIntosh <geoff@mac.into.sh>
2020-06-04 17:19:52 -0230

    Clean comments in init

--- emacs/init.el
+++ emacs/init.el
@@ -27,7 +27,6 @@ identically to setq, setting VARIABLE to VALUE."
   (use-package no-littering
     :straight t)
   (straight-use-package '(org-plus-contrib :includes (org)))
-  ;;(straight-use-package 'org)
   (mapc #'org-babel-load-file
         (directory-files user-emacs-directory t "\\.org$"))
   (garbage-collect))
