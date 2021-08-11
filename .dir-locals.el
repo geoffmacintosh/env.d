1bdfe84ef2834b05fa57661066bc57c77e2bd697
Geoff MacIntosh <geoff@mac.into.sh>
2021-08-11 13:44:52 -0600

    Did I forget to commit for a while? Yes.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((org-archive-location . "archive.org::datetree/* From %s"))))
+;; ((org-mode . ((org-archive-location . "archive.org::datetree/* From %s"))))
